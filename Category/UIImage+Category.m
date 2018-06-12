ee7762585890e1ba95ed7beb3be13ae5ca509135
yunxin bai <bbbaiyunxin@gmail.com>
2018-06-12 09:32:09 +0800

    新增 UIImage 分类

---
+++ Category/UIImage+Category.m
@@ -0,0 +1,34 @@
+//
+//  UIImage+Category.m
+//  JPlus_Objective-C
+//
+//  Created by yunxin bai on 2018/2/26.
+//  Copyright © 2018年 yunxin bai. All rights reserved.
+//
+
+#import "UIImage+Category.h"
+
+@implementation UIImage (Category)
+
++ (instancetype)yx_imageWithOriginal:(NSString *)imageName{
+    
+    UIImage *image = [UIImage imageNamed:imageName];
+    return [image imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+}
+
+
+
++ (instancetype)yx_imageWithColor:(UIColor *)color{
+    
+    CGRect rect = CGRectMake(0.0f, 0.0f, 1.0f, 1.0f);
+    UIGraphicsBeginImageContext(rect.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    CGContextSetFillColorWithColor(context, [color CGColor]);
+    CGContextFillRect(context, rect);
+    
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return image;
+}
