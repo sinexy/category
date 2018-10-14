5a3de4e1c8601e24d7165079613eac746900637c
yunxin bai <bbbaiyunxin@gmail.com>
2018-10-14 10:56:04 +0800

    fixed bug

--- Category/Foundation/NSData+BYXHash.h
+++ Category/Foundation/NSData+BYXHash.h
@@ -1,14 +1,8 @@
-//
-//  NSData+JKHash.h
-//  JKCategories (https://github.com/shaojiankui/JKCategories)
-//
-//  Created by Jakey on 15/6/1.
-//  Copyright (c) 2015年 www.skyfox.org. All rights reserved.
-//
+
 
 #import <Foundation/Foundation.h>
 
-@interface NSData (JKHash)
+@interface NSData (BYXHash)
 /**
  *  @brief  md5 NSData
  */
