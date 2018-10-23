f07ddcfe8d5db78a7405a33bf78e235129e15f29
yunxin bai <bbbaiyunxin@gmail.com>
2018-10-23 11:42:23 +0800

    修改常量名

--- Category/UIKit/UIView/UIView+BYXToast.m
+++ Category/UIKit/UIView/UIView+BYXToast.m
@@ -9,46 +9,46 @@
 #import "UIView+BYXToast.h"
 #import <objc/runtime.h>
 // display duration
-static const NSTimeInterval BYXToastDefaultDuration  = 3.f;
+static const NSTimeInterval kBYXToastDefaultDuration  = 3.f;
 
 // general appearance
-static const CGFloat BYXToastMaxWidth            = 0.8;      // 80% of parent view width
-static const CGFloat BYXToastMaxHeight           = 0.8;      // 80% of parent view height
-static const CGFloat BYXToastHorizontalPadding   = 10.f;
-static const CGFloat BYXToastVerticalPadding     = 10.f;
-static const CGFloat BYXToastCornerRadius        = 10.f;
-static const CGFloat BYXToastOpacity             = 0.8;
-static const CGFloat BYXToastFontSize            = 16.f;
-static const CGFloat BYXToastMaxTitleLines       = 0;
-static const CGFloat BYXToastMaxMessageLines     = 0;
-static const NSTimeInterval BYXToastFadeDuration = 0.2;
+static const CGFloat kBYXToastMaxWidth            = 0.8;      // 80% of parent view width
+static const CGFloat kBYXToastMaxHeight           = 0.8;      // 80% of parent view height
+static const CGFloat kBYXToastHorizontalPadding   = 10.f;
+static const CGFloat kBYXToastVerticalPadding     = 10.f;
+static const CGFloat kBYXToastCornerRadius        = 10.f;
+static const CGFloat kBYXToastOpacity             = 0.8;
+static const CGFloat kBYXToastFontSize            = 16.f;
+static const CGFloat kBYXToastMaxTitleLines       = 0;
+static const CGFloat kBYXToastMaxMessageLines     = 0;
+static const NSTimeInterval kBYXToastFadeDuration = 0.2;
 
 // shadow appearance
-static const CGFloat BYXToastShadowOpacity       = 0.8;
-static const CGFloat BYXToastShadowRadius        = 6.f;
-static const CGSize  BYXToastShadowOffset        = { 4.f, 4.f };
-static const BOOL    BYXToastDisplayShadow       = YES;
+static const CGFloat kBYXToastShadowOpacity       = 0.8;
+static const CGFloat kBYXToastShadowRadius        = 6.f;
+static const CGSize  kBYXToastShadowOffset        = { 4.f, 4.f };
+static const BOOL    kBYXToastDisplayShadow       = YES;
 
 // image view size
-static const CGFloat BYXToastImageViewWidth      = 80.f;
-static const CGFloat BYXToastImageViewHeight     = 80.f;
+static const CGFloat kBYXToastImageViewWidth      = 80.f;
+static const CGFloat kBYXToastImageViewHeight     = 80.f;
 
 // activity
-static const CGFloat BYXToastActivityWidth       = 100.f;
-static const CGFloat BYXToastActivityHeight      = 100.f;
-static const NSString * BYXToastActivityDefaultPosition = @"center";
+static const CGFloat kBYXToastActivityWidth       = 100.f;
+static const CGFloat kBYXToastActivityHeight      = 100.f;
+static const NSString * kBYXToastActivityDefaultPosition = @"kcenter";
 
 // interaction
-static const BOOL BYXToastHidesOnTap             = YES;     // excludes activity views
+static const BOOL kBYXToastHidesOnTap             = YES;     // excludes activity views
 
 // associative reference keys
-static const NSString * BYXToastTimerKey         = @"BYXToastTimerKey";
-static const NSString * BYXToastActivityViewKey  = @"BYXToastActivityViewKey";
-static const NSString * BYXToastTapCallbackKey   = @"BYXToastTapCallbackKey";
+static const NSString * kBYXToastTimerKey         = @"kBYXToastTimerKey";
+static const NSString * kBYXToastActivityViewKey  = @"kBYXToastActivityViewKey";
+static const NSString * kBYXToastTapCallbackKey   = @"kBYXToastTapCallbackKey";
 @implementation UIView (BYXToast)
 
 - (void)byx_makeToast:(NSString *)message {
-    [self byx_makeToast:message duration:BYXToastDefaultDuration postion:BYXToastPositionCenter];
+    [self byx_makeToast:message duration:kBYXToastDefaultDuration postion:BYXToastPositionCenter];
 }
 
 - (void)byx_makeToast:(NSString *)message duration:(NSTimeInterval)duration postion:(BYXToastPosition)position {
@@ -75,7 +75,7 @@ static const NSString * BYXToastTapCallbackKey   = @"BYXToastTapCallbackKey";
 
 
 - (void)byx_showToast:(UIView *)toast {
-    [self byx_showToast:toast duration:BYXToastDefaultDuration position:BYXToastPositionCenter];
+    [self byx_showToast:toast duration:kBYXToastDefaultDuration position:BYXToastPositionCenter];
 }
 
 - (void)byx_showToast:(UIView *)toast duration:(NSTimeInterval)duration position:(BYXToastPosition)position {
@@ -86,7 +86,7 @@ static const NSString * BYXToastTapCallbackKey   = @"BYXToastTapCallbackKey";
     toast.center = [self byx_centerPointForPosition:position withToast:toast];
     toast.alpha = 0.f;
     
-    if (BYXToastHidesOnTap) {
+    if (kBYXToastHidesOnTap) {
         UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:toast action:@selector(byx_handleToastTapped:)];
         [toast addGestureRecognizer:tap];
         toast.userInteractionEnabled = YES;
@@ -94,17 +94,17 @@ static const NSString * BYXToastTapCallbackKey   = @"BYXToastTapCallbackKey";
     }
     [self addSubview:toast];
     
-    [UIView animateWithDuration:BYXToastFadeDuration delay:0.f options:(UIViewAnimationOptionCurveEaseOut | UIViewAnimationOptionAllowUserInteraction) animations:^{
+    [UIView animateWithDuration:kBYXToastFadeDuration delay:0.f options:(UIViewAnimationOptionCurveEaseOut | UIViewAnimationOptionAllowUserInteraction) animations:^{
         toast.alpha = 1.f;
     } completion:^(BOOL finished) {
         NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:duration target:self selector:@selector(byx_toastTimerDidFinish:) userInfo:toast repeats:NO];
-        objc_setAssociatedObject(toast, &BYXToastTimerKey, timer, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-        objc_setAssociatedObject(toast, &BYXToastTapCallbackKey, tapCallback, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+        objc_setAssociatedObject(toast, &kBYXToastTimerKey, timer, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+        objc_setAssociatedObject(toast, &kBYXToastTapCallbackKey, tapCallback, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
     }];
 }
 
 - (void)byx_hideToast:(UIView *)toast {
-    [UIView animateWithDuration:BYXToastFadeDuration delay:0.f options:(UIViewAnimationOptionCurveEaseIn | UIViewAnimationOptionBeginFromCurrentState) animations:^{
+    [UIView animateWithDuration:kBYXToastFadeDuration delay:0.f options:(UIViewAnimationOptionCurveEaseIn | UIViewAnimationOptionBeginFromCurrentState) animations:^{
         toast.alpha = 0.f;
     } completion:^(BOOL finished) {
         [toast removeFromSuperview];
@@ -116,10 +116,10 @@ static const NSString * BYXToastTapCallbackKey   = @"BYXToastTapCallbackKey";
 }
 
 - (void)byx_handleToastTapped:(UITapGestureRecognizer *)tap {
-    NSTimer *timer = objc_getAssociatedObject(self, &BYXToastTimerKey);
+    NSTimer *timer = objc_getAssociatedObject(self, &kBYXToastTimerKey);
     [timer invalidate];
     
-    void (^callback)(void) = objc_getAssociatedObject(self, &BYXToastTapCallbackKey);
+    void (^callback)(void) = objc_getAssociatedObject(self, &kBYXToastTapCallbackKey);
     if (callback) {
         callback();
     }
@@ -131,21 +131,21 @@ static const NSString * BYXToastTapCallbackKey   = @"BYXToastTapCallbackKey";
 }
 
 - (void)byx_makeToastActivity:(BYXToastPosition)position {
-    UIView *existingActivityView = objc_getAssociatedObject(self, &BYXToastActivityViewKey);
+    UIView *existingActivityView = objc_getAssociatedObject(self, &kBYXToastActivityViewKey);
     if (existingActivityView) return;
     
-    UIView *activityView = [[UIView alloc] initWithFrame:CGRectMake(0.f, 0.f, BYXToastActivityWidth, BYXToastActivityHeight)];
+    UIView *activityView = [[UIView alloc] initWithFrame:CGRectMake(0.f, 0.f, kBYXToastActivityWidth, kBYXToastActivityHeight)];
     activityView.center = [self byx_centerPointForPosition:position withToast:activityView];
-    activityView.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:BYXToastOpacity];
+    activityView.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:kBYXToastOpacity];
     activityView.alpha = 0.f;
     activityView.autoresizingMask = (UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin);
-    activityView.layer.cornerRadius = BYXToastCornerRadius;
+    activityView.layer.cornerRadius = kBYXToastCornerRadius;
     
-    if (BYXToastDisplayShadow) {
+    if (kBYXToastDisplayShadow) {
         activityView.layer.shadowColor = [UIColor blackColor].CGColor;
-        activityView.layer.shadowOpacity = BYXToastShadowOpacity;
-        activityView.layer.shadowRadius = BYXToastShadowRadius;
-        activityView.layer.shadowOffset = BYXToastShadowOffset;
+        activityView.layer.shadowOpacity = kBYXToastShadowOpacity;
+        activityView.layer.shadowRadius = kBYXToastShadowRadius;
+        activityView.layer.shadowOffset = kBYXToastShadowOffset;
     }
     
     UIActivityIndicatorView *activityIndicatorView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
@@ -153,11 +153,11 @@ static const NSString * BYXToastTapCallbackKey   = @"BYXToastTapCallbackKey";
     [activityView addSubview:activityIndicatorView];
     [activityIndicatorView startAnimating];
     
-    objc_setAssociatedObject (self, &BYXToastActivityViewKey, activityView, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    objc_setAssociatedObject (self, &kBYXToastActivityViewKey, activityView, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
     
     [self addSubview:activityView];
     
-    [UIView animateWithDuration:BYXToastFadeDuration
+    [UIView animateWithDuration:kBYXToastFadeDuration
                           delay:0.0
                         options:UIViewAnimationOptionCurveEaseOut
                      animations:^{
@@ -166,28 +166,28 @@ static const NSString * BYXToastTapCallbackKey   = @"BYXToastTapCallbackKey";
 }
 
 - (void)byx_hideToastActivity {
-    UIView *existingActivityView = (UIView *)objc_getAssociatedObject(self, &BYXToastActivityViewKey);
+    UIView *existingActivityView = (UIView *)objc_getAssociatedObject(self, &kBYXToastActivityViewKey);
     if (existingActivityView != nil) {
-        [UIView animateWithDuration:BYXToastFadeDuration
+        [UIView animateWithDuration:kBYXToastFadeDuration
                               delay:0.f
                             options:(UIViewAnimationOptionCurveEaseIn | UIViewAnimationOptionBeginFromCurrentState)
                          animations:^{
                              existingActivityView.alpha = 0.f;
                          } completion:^(BOOL finished) {
                              [existingActivityView removeFromSuperview];
-                             objc_setAssociatedObject (self, &BYXToastActivityViewKey, nil, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+                             objc_setAssociatedObject (self, &kBYXToastActivityViewKey, nil, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
                          }];
     }
 }
 
 - (CGPoint)byx_centerPointForPosition:(BYXToastPosition)position withToast:(UIView *)toast {
     if (position == BYXToastPositionTop) {
-        return CGPointMake(self.bounds.size.width/2, (toast.frame.size.height / 2) + BYXToastVerticalPadding);
+        return CGPointMake(self.bounds.size.width/2, (toast.frame.size.height / 2) + kBYXToastVerticalPadding);
     }else if (position == BYXToastPositionCenter) {
         return CGPointMake(self.bounds.size.width / 2, self.bounds.size.height / 2);
 
     }else {
-        return CGPointMake(self.bounds.size.width/2, (self.bounds.size.height - (toast.frame.size.height / 2)) - BYXToastVerticalPadding);
+        return CGPointMake(self.bounds.size.width/2, (self.bounds.size.height - (toast.frame.size.height / 2)) - kBYXToastVerticalPadding);
     }
 }
 
@@ -218,21 +218,21 @@ static const NSString * BYXToastTapCallbackKey   = @"BYXToastTapCallbackKey";
     wrapperView.autoresizingMask = (
                                     UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin
                                     );
-    wrapperView.layer.cornerRadius = BYXToastCornerRadius;
+    wrapperView.layer.cornerRadius = kBYXToastCornerRadius;
     
-    if (BYXToastDisplayShadow) {
+    if (kBYXToastDisplayShadow) {
         wrapperView.layer.shadowColor = [UIColor blackColor].CGColor;
-        wrapperView.layer.shadowOpacity = BYXToastShadowOpacity;
-        wrapperView.layer.shadowRadius = BYXToastShadowRadius;
-        wrapperView.layer.shadowOffset = BYXToastShadowOffset;
+        wrapperView.layer.shadowOpacity = kBYXToastShadowOpacity;
+        wrapperView.layer.shadowRadius = kBYXToastShadowRadius;
+        wrapperView.layer.shadowOffset = kBYXToastShadowOffset;
     }
     
-    wrapperView.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:BYXToastOpacity];
+    wrapperView.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:kBYXToastOpacity];
     
     if (image) {
         imageView = [[UIImageView alloc] initWithImage:image];
         imageView.contentMode = UIViewContentModeScaleAspectFit;
-        imageView.frame = CGRectMake(BYXToastHorizontalPadding, BYXToastVerticalPadding, BYXToastImageViewWidth, BYXToastImageViewHeight);
+        imageView.frame = CGRectMake(kBYXToastHorizontalPadding, kBYXToastVerticalPadding, kBYXToastImageViewWidth, kBYXToastImageViewHeight);
     }
     
     CGFloat imageWidth, imageHeight, imageLeft;
@@ -240,15 +240,15 @@ static const NSString * BYXToastTapCallbackKey   = @"BYXToastTapCallbackKey";
     if (imageView) {
         imageWidth = imageView.bounds.size.width;
         imageHeight = imageView.bounds.size.height;
-        imageLeft = BYXToastHorizontalPadding;
+        imageLeft = kBYXToastHorizontalPadding;
     } else {
         imageWidth = imageHeight = imageLeft = 0.f;
     }
     
     if (title) {
         titleLabel = [[UILabel alloc] init];
-        titleLabel.numberOfLines = BYXToastMaxTitleLines;
-        titleLabel.font = [UIFont boldSystemFontOfSize:BYXToastFontSize];
+        titleLabel.numberOfLines = kBYXToastMaxTitleLines;
+        titleLabel.font = [UIFont boldSystemFontOfSize:kBYXToastFontSize];
         titleLabel.textAlignment = NSTextAlignmentCenter;
         titleLabel.lineBreakMode = NSLineBreakByWordWrapping;
         titleLabel.textColor = [UIColor whiteColor];
@@ -256,22 +256,22 @@ static const NSString * BYXToastTapCallbackKey   = @"BYXToastTapCallbackKey";
         titleLabel.alpha = 1.f;
         titleLabel.text = title;
         
-        CGSize maxSizeTitle = CGSizeMake((self.bounds.size.width*BYXToastMaxWidth)-imageWidth, self.bounds.size.height*BYXToastMaxHeight);
+        CGSize maxSizeTitle = CGSizeMake((self.bounds.size.width*kBYXToastMaxWidth)-imageWidth, self.bounds.size.height*kBYXToastMaxHeight);
         CGSize expectedSizeTitle = [self byx_sizeForString:title font:titleLabel.font constrainedToSize:maxSizeTitle lineBreakMode:titleLabel.lineBreakMode];
         titleLabel.frame = CGRectMake(0.f, 0.f, expectedSizeTitle.width, expectedSizeTitle.height);
     }
     
     if (message) {
         messageLabel = [[UILabel alloc] init];
-        messageLabel.numberOfLines = BYXToastMaxMessageLines;
-        messageLabel.font = [UIFont systemFontOfSize:BYXToastFontSize];
+        messageLabel.numberOfLines = kBYXToastMaxMessageLines;
+        messageLabel.font = [UIFont systemFontOfSize:kBYXToastFontSize];
         messageLabel.lineBreakMode = NSLineBreakByWordWrapping;
         messageLabel.textColor = [UIColor whiteColor];
         messageLabel.backgroundColor = [UIColor clearColor];
         messageLabel.alpha = 1.f;
         messageLabel.text = message;
         
-        CGSize maxSizeMessage = CGSizeMake((self.bounds.size.width * BYXToastMaxWidth) - imageWidth, self.bounds.size.height * BYXToastMaxHeight);
+        CGSize maxSizeMessage = CGSizeMake((self.bounds.size.width * kBYXToastMaxWidth) - imageWidth, self.bounds.size.height * kBYXToastMaxHeight);
         CGSize expectedSizeMessage = [self byx_sizeForString:message font:messageLabel.font constrainedToSize:maxSizeMessage lineBreakMode:messageLabel.lineBreakMode];
         messageLabel.frame = CGRectMake(0.f, 0.f, expectedSizeMessage.width, expectedSizeMessage.height);
     }
@@ -281,8 +281,8 @@ static const NSString * BYXToastTapCallbackKey   = @"BYXToastTapCallbackKey";
     if (titleLabel) {
         titleWidth = titleLabel.bounds.size.width;
         titleHeight = titleLabel.bounds.size.height;
-        titleTop = BYXToastVerticalPadding;
-        titleLeft = imageLeft + imageWidth + BYXToastHorizontalPadding;
+        titleTop = kBYXToastVerticalPadding;
+        titleLeft = imageLeft + imageWidth + kBYXToastHorizontalPadding;
     } else {
         titleWidth = titleHeight = titleTop = titleLeft = 0.f;
     }
@@ -292,8 +292,8 @@ static const NSString * BYXToastTapCallbackKey   = @"BYXToastTapCallbackKey";
     if(messageLabel != nil) {
         messageWidth = messageLabel.bounds.size.width;
         messageHeight = messageLabel.bounds.size.height;
-        messageLeft = imageLeft + imageWidth + BYXToastHorizontalPadding;
-        messageTop = titleTop + titleHeight + BYXToastVerticalPadding;
+        messageLeft = imageLeft + imageWidth + kBYXToastHorizontalPadding;
+        messageTop = titleTop + titleHeight + kBYXToastVerticalPadding;
     } else {
         messageWidth = messageHeight = messageLeft = messageTop = 0.0;
     }
@@ -302,8 +302,8 @@ static const NSString * BYXToastTapCallbackKey   = @"BYXToastTapCallbackKey";
     CGFloat longerLeft = MAX(titleLeft, messageLeft);
     
     // wrapper width uses the longerWidth or the image width, whatever is larger. same logic applies to the wrapper height
-    CGFloat wrapperWidth = MAX((imageWidth + (BYXToastHorizontalPadding * 2)), (longerLeft + longerWidth + BYXToastHorizontalPadding));
-    CGFloat wrapperHeight = MAX((messageTop + messageHeight + BYXToastVerticalPadding), (imageHeight + (BYXToastVerticalPadding * 2)));
+    CGFloat wrapperWidth = MAX((imageWidth + (kBYXToastHorizontalPadding * 2)), (longerLeft + longerWidth + kBYXToastHorizontalPadding));
+    CGFloat wrapperHeight = MAX((messageTop + messageHeight + kBYXToastVerticalPadding), (imageHeight + (kBYXToastVerticalPadding * 2)));
     
     wrapperView.frame = CGRectMake(0.0, 0.0, wrapperWidth, wrapperHeight);
     
