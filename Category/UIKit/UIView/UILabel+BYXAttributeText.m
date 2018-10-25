4236daf645422b6d0009b1959ba1ba57e895ed09
yunxin bai <bbbaiyunxin@gmail.com>
2018-10-25 14:03:08 +0800

    新增 富文本 label 的 category

--- Category/UIKit/UIView/UILabel+BYXAttributeText.m
+++ Category/UIKit/UIView/UILabel+BYXAttributeText.m
@@ -7,7 +7,9 @@
 //
 
 #import "UILabel+BYXAttributeText.h"
+#import <objc/runtime.h>
 
+static NSString *kBYXAttributeTextAreaKey = @"kBYXAttributeTextAreaKey";
 
 
 @implementation UILabel (BYXAttributeText)
@@ -54,51 +56,19 @@
 
 - (void)byx_appendText:(NSString *)text color:(UIColor *)color font:(UIFont *)font styles:(BYXAttributeName)style tapEvent:(void (^)(void))tapEvent {
     
-    NSMutableAttributedString *attributedString = [NSMutableAttributedString new];
-    NSAttributedString *oldString;
-    
-    UIColor *defaultColor = self.textColor;
-    
-    if (self.attributedText.length) {
-        oldString = [self.attributedText mutableCopy];
-    } else if (self.text.length) {
-        oldString = [[NSAttributedString alloc] initWithString:self.text];
-    } else {
-        oldString = [[NSAttributedString alloc] initWithString:@""];
-    }
-    NSRange range = NSMakeRange(0, oldString.length);
-    NSDictionary *dic = [oldString attributesAtIndex:0 effectiveRange:&range];
-    
-    [attributedString appendAttributedString:oldString];
-    [attributedString appendAttributedString:[[NSAttributedString alloc] initWithString:text]];
-
-    if (dic) {
-        [attributedString addAttributes:dic range:range];
-    }
-    
-    if (color) {
-        [attributedString addAttribute:NSForegroundColorAttributeName value:color range:NSMakeRange(oldString.length, text.length)];
-    }
-    
-    if (font) {
-        [attributedString addAttribute:NSFontAttributeName value:font range:NSMakeRange(oldString.length, text.length)];
-    }
-    
-    if (style == BYXAttributeNameUnderline) {
-        [attributedString addAttribute:NSStrikethroughStyleAttributeName value:@(1) range:NSMakeRange(oldString.length, text.length)];
-        [attributedString addAttribute:NSStrikethroughColorAttributeName value:color?:defaultColor range:NSMakeRange(oldString.length, text.length)];
-    }else if (style == BYXAttributeNameStrikethrough) {
-        [attributedString addAttribute:NSUnderlineStyleAttributeName value:@(1) range:NSMakeRange(oldString.length, text.length)];
-        [attributedString addAttribute:NSUnderlineColorAttributeName value:color?:defaultColor range:NSMakeRange(oldString.length, text.length)];
-    }
-    CGRect rect = [self textRectForBounds:self.frame limitedToNumberOfLines:0];
-    NSLog(@"%@", NSStringFromCGRect(rect));
+    NSMutableAttributedString *attributedString = [self getOriginalAttributeString];
+    NSAttributedString *newAttributedString = [self getNewAttributeString:text color:color font:font styles:style];
+    [attributedString appendAttributedString:newAttributedString];
     
     if (tapEvent) {
         self.userInteractionEnabled = YES;
-        tapEvent();
+    }else {
+        tapEvent = ^{
+            
+        };
     }
-    self.text = @"";
+    [self setTapRect:text font:font styles:style tapEvent:tapEvent];
+
     self.attributedText = attributedString;
 }
 
@@ -108,7 +78,127 @@
     UITouch *touch = array.firstObject;
     CGPoint point = [touch locationInView:self];
     NSLog(@"%f,%f",point.x, point.y);
+    void (^blockName)(void);
+    
+    
+    NSMutableArray *textArrayM = objc_getAssociatedObject(self, &kBYXAttributeTextAreaKey);
+    for (NSArray *array in textArrayM) {
+        blockName = array[2];
+        NSArray *rectArray = array[1];
+        for (id r in rectArray) {
+            CGRect rect = [r CGRectValue];
+            if ((rect.origin.x < point.x && (rect.size.width+rect.origin.x) > point.x) && (rect.origin.y < point.y && (rect.size.height+rect.origin.y) > point.y)) {  // 点在区域内
+                NSLog(@"%@",array[0]);
+                blockName();
+                return;
+            }
+        }
+
+    }
+    
+    
 }
 
+
+#pragma mark - private method
+
+- (NSMutableAttributedString *)getOriginalAttributeString {
+    NSMutableAttributedString *attributedString;
+    if (self.attributedText.length) {
+        attributedString = [self.attributedText mutableCopy];
+    } else if (self.text.length) {
+        attributedString = [[NSMutableAttributedString alloc] initWithString:self.text];
+    } else {
+        attributedString = [NSMutableAttributedString new];
+    }
+    if (attributedString.length == 0) {
+        return attributedString;
+    }
+    NSRange range = NSMakeRange(0, attributedString.length);
+    NSDictionary *dic = [attributedString attributesAtIndex:0 effectiveRange:&range];
+    if (dic) {
+        [attributedString addAttributes:dic range:range];
+    }
+    return attributedString;
+}
+
+- (NSAttributedString *)getNewAttributeString:(NSString *)text color:(UIColor *)color font:(UIFont *)font styles:(BYXAttributeName)style {
+
+    NSMutableDictionary *attributeDiction = [NSMutableDictionary dictionary];
+    if (color) {
+        [attributeDiction setValue:color forKey:NSForegroundColorAttributeName];
+    }
+    if (font) {
+        [attributeDiction setValue:font forKey:NSFontAttributeName];
+    }
+    if (style == BYXAttributeNameUnderline) {
+        [attributeDiction setValue:@(1) forKey:NSStrikethroughStyleAttributeName];
+        [attributeDiction setValue:color?:self.textColor forKey:NSStrikethroughColorAttributeName];
+    }else if (style == BYXAttributeNameStrikethrough) {
+        [attributeDiction setValue:@(1) forKey:NSUnderlineStyleAttributeName];
+        [attributeDiction setValue:color?:self.textColor forKey:NSUnderlineColorAttributeName];
+    }
+    NSAttributedString *attributeString = [[NSAttributedString alloc] initWithString:text attributes:attributeDiction];
+    return attributeString;
+}
+
+- (void)setTapRect:(NSString *)text font:(UIFont *)font styles:(BYXAttributeName)style tapEvent:(void (^)(void))tapEvent {
+    
+    NSMutableArray *textArrayM = objc_getAssociatedObject(self, &kBYXAttributeTextAreaKey);
+    if (!textArrayM) {
+        textArrayM = [NSMutableArray array];
+        objc_setAssociatedObject(self, &kBYXAttributeTextAreaKey, textArrayM, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    }
+    
+    NSArray *oldRectArray = [textArrayM lastObject][1];
+    CGRect lastRect = CGRectZero;
+    for (id r in oldRectArray) {
+        lastRect = [r CGRectValue];
+    }
+    
+    CGSize textSize = [text boundingRectWithSize:CGSizeMake(MAXFLOAT, 40) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName: font?:self.font} context:nil].size;
+    CGFloat x = lastRect.origin.x + lastRect.size.width;
+    CGFloat soloWidth = round(textSize.width / text.length);
+    NSInteger line = (x + textSize.width) / self.frame.size.width;
+    CGFloat width = round(textSize.width);
+    CGFloat height = round(textSize.height);
+    
+    NSMutableArray *rectArrayM = [NSMutableArray array];
+    CGRect rect;
+    if (line > 0) {
+
+        width = 0;
+        while (x + width + soloWidth <= self.frame.size.width) {
+            width += soloWidth;
+        }
+        int i = 0;
+        do {
+            rect = CGRectMake(x, lastRect.origin.y + i * height, width, height);
+            [rectArrayM addObject:@(rect)];
+            i++;
+            x = 0;
+            width = textSize.width - width;
+            int currentLine = width / self.frame.size.width;
+            if (currentLine > 0) {
+                width = 0;
+                while (x + width + soloWidth <= self.frame.size.width) {
+                    width += soloWidth;
+                }
+            }
+            
+        } while (i <= line || width > self.frame.size.width);
+        
+    }else {
+        rect = CGRectMake(x, line*height, width, height);
+        [rectArrayM addObject:@(rect)];
+    }
+    
+    [textArrayM addObject:@[text,rectArrayM,tapEvent]];
+
+}
+
+- (void)getLastRect:(NSString *)text {
+    
+}
 
 @end
